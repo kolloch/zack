d23bd85ffba1a7e20fdadb92cd6b11f7f847d22a
Peter Kolloch <info@eigenvalue.net>
2025-02-22 18:44:08 +0100

    docs: adjust base

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -4,6 +4,8 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://kolloch.github.io',
+	base: 'zack',
 	integrations: [
 		starlight({
 			title: 'Zack',
