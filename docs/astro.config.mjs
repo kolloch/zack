253989d03d9c657b4c851b80605b2970a646856a
Peter Kolloch <info@eigenvalue.net>
2025-04-05 16:44:25 +0200

    attempted page build fix

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -9,6 +9,11 @@ import markdoc from '@astrojs/markdoc';
 export default defineConfig({
     site: 'https://kolloch.github.io',
     base: 'zack',
+    i18n: {
+        routing: {
+            redirectToDefaultLocale: undefined,
+        }
+    },
     integrations: [starlight({
         plugins: [starlightLinksValidator()],
         title: 'Zack Build',
